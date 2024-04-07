1342b02044641a27aebf37ca0e8ce560314d69e9
handidan <handikadika896@gmail.com>
2024-04-07 23:38:52 +0700

    update map

--- Assets/Script/Player_Controller.cs
+++ Assets/Script/Player_Controller.cs
@@ -5,58 +5,23 @@ using UnityEngine;
 public class Player_Controller : MonoBehaviour
 {
     public float moveSpeed;
-    public float runSpeed = 5f;
     public Rigidbody theRB;
     public float jumpForce;
-    public Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        // theRB = GetComponent<Rigidbody>();
+        theRB = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
-        if (h != 0 || v != 0)
-        {
-            animator.SetBool("Walking", true);
-            if (Input.GetKey(KeyCode.LeftShift))
-            {
-                animator.SetBool("Running", true);
-            }
-            else
-            {
-                animator.SetBool("Running", false);
-            }
-        }
-        else
-        {
-            animator.SetBool("Walking", false);
-            animator.SetBool("Running", false);
-        }
-        Vector3 movement = new Vector3(h, 0.0f, v);
-        if (movement != new Vector3(0.0f, 0.0f, 0.0f))
-        {
-            transform.rotation = Quaternion.LookRotation(movement);
-        }
-
-        if (Input.GetKey(KeyCode.LeftShift)) // running
-        {
-            theRB.velocity = new Vector3(Input.GetAxis("Horizontal") * (moveSpeed + runSpeed), theRB.velocity.y, Input.GetAxis("Vertical") * (moveSpeed + runSpeed));
-        }
-        else // walking
-        {
-            theRB.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, theRB.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
-        }
-
+        theRB.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, theRB.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
 
         if (Input.GetButtonDown("Jump"))
         {
             theRB.velocity = new Vector3(theRB.velocity.x, jumpForce, theRB.velocity.z);
-        }
+        }  
     }
 }
